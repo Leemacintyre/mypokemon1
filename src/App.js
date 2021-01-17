bf1fdbb0e83a07934e6a60cee8e0b11a8ef57239
Lee Macintyre <macintyrelee@gmail.com>
2021-01-17 07:21:50 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import './app.css';
-import { Route, Switch } from 'react-router-dom';
-import { Pokedex } from './pokedex/pokedex';
-import { Pokemon } from './pokemon/pokemon';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div className='appContainer'>
-			<Switch>
-				<Route exact path='/' render={(props) => <Pokedex {...props} />} />
-				<Route
-					exact
-					path='/:pokemonId'
-					render={(props) => <Pokemon {...props} />}
-				/>
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import './Pokemon Hollow.ttf';
-import './index.css';
-
-const history = createBrowserHistory();
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Router history={history}>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
